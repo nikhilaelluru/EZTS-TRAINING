77c59770d08f609626e77c8827da5c5c9a6ec804
nikhilaelluru <162121659+nikhilaelluru@users.noreply.github.com>
2024-06-19 16:01:50 +0530

    Update sliding_window.py

--- sliding_window.py
+++ sliding_window.py
@@ -1,4 +1,8 @@
 #SLIDING WINDOW:
+# Given an interger array your task is to find the K continious digits which gives you the maximum sum.
+# Where K is entered by user
+# sample input [2,4,3,5,6,3,4,6,7,1,2,5]
+# output: [4,6,7]
 
 def max_sum(k,lst):
     window=max=0
