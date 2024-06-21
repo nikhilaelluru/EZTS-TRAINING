1bdc0b56acbf263a2419db72cd009f0c7717d61a
nikhilaelluru <162121659+nikhilaelluru@users.noreply.github.com>
2024-06-21 22:58:08 +0530

    Add files via upload

---
+++ dog_age.py
@@ -0,0 +1,17 @@
+#DOG AGE
+
+# Max has a dog, which is an integer N years old. Now he wants the age of his dog in human years. The internet says 
+# that 1 dog year equals to 7 human years. Your task is to find and return an integer value representing the age of 
+# Max's dog in human years.
+
+# Input Format:
+# input1: An integer value N representing the age of Max's dog
+ 
+# Output Format: Return an integer value representing the age of Max's dog in human years
+
+'''
+def dog_age(n):
+    return n*7
+n=int(input())
+print(dog_age(n))
+'''
