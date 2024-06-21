5e06ad3623cacb02f9392022bd9b20d38bdb440f
nikhilaelluru <162121659+nikhilaelluru@users.noreply.github.com>
2024-06-21 23:20:12 +0530

    Add files via upload

---
+++ chess_board.py
@@ -0,0 +1,29 @@
+#CHESS BOARD
+
+# you are given coordinates, a string that represents coordinates of square of the chessboard. Below is a chessboard for your reference.
+# Return true if the square is white, and false if the sqaure is black.
+# The coordinates will always represent a valid chessboard square. The coordinate will always have the letter first and number second.
+
+# Example: input: coordinates="a1"
+#          Output: false
+
+'''
+evenrows="2468"
+oddrows="1357"
+
+evencol="bdfh"
+oddcol="aceg"
+
+n="h6"
+
+if n[0] in evencol:
+    if n[1] in evenrows:
+        print("black")
+    else:
+        print("white")
+else:
+    if n[1] in evenrows:
+        print("white")
+    else:
+        print("black")
+ '''       
