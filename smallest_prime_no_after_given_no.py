c5f076eead9e8370cc8e4297de2b0e48f35a3216
nikhilaelluru <162121659+nikhilaelluru@users.noreply.github.com>
2024-06-21 22:58:25 +0530

    Add files via upload

---
+++ smallest_prime_no_after_given_no.py
@@ -0,0 +1,17 @@
+# input format: an integer value N
+# output format: return an integer value representing the smallest prime number after that number
+
+'''
+def isprime(n):
+    for i in range(2,(n//2)+1):
+        if n%i==0:
+            return False
+    return True
+num=int(input())
+k=num+1
+while True:
+    if isprime(k):
+        print(k)
+        break
+    k=k+1
+    '''
